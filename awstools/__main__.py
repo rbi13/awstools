6bbd9a2d78352383bf14532721f594f361b0ecd0
Ron <ron@ron.local>
2017-10-30 17:42:27 -0400

    project skeleton

---
+++ awstools/__main__.py
@@ -0,0 +1,10 @@
+import sys
+import argparse
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='awstools',
+      version='0.1',
+      description='various scripts for managing aws deployments',
+      url='http://github.com/hubba/awstools',
+      author='rbi13',
+      license='MIT',
+      packages=['awstools'],
+      install_requires=[
+          'boto3',
+          'argparse'
+      ],
+      entry_points={
+          'console_scripts': [
+              'awstools = awstools.__main__:main'
+          ]
+      },
+      zip_safe=False)
